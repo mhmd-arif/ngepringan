c63305e381871849aa0fdba26a1c4b2bd04547a0
usernamemu <mhmdarif.developer@gmail.com>
2023-08-01 22:13:46 +0700

    pertanianmikro

--- client/src/pages/blog/EduPertanianMikro.jsx
+++ client/src/pages/blog/EduPertanianMikro.jsx
@@ -1,6 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import { Bg, GG1, GG2, GG3, GG4, GG5, Logo } from "../../assets";
+import {
+  tani1,
+  tani2,
+  tani3,
+  tani4,
+  galon,
+  pisau,
+  solder,
+} from "../../assets/image";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 
@@ -14,109 +23,105 @@ const EduPertanianMikro = () => {
         id="Body"
         className=" w-full flex flex-col container mx-auto font-poppins items-center lg:text-center lg:mt-32 font-bold max-w-5xl"
       >
-        <h1 className="text-4xl mb-10">ECO BRICK</h1>
+        <h1 className="text-4xl mb-10">PERTANIAN MIKRO</h1>
         <div className="max-w-4xl flex flex-col gap-14">
-          <div className="flex flex-col gap-2 ">
+          <div className="flex flex-col gap-2 items-center">
             <h2>TAUKAH KAMU?</h2>
-            <p className=" text-sm font-semibold">
-              Dari setiap botol air mineral yang kita gunakan setiap hari, kita
-              memiliki kesempatan untuk menciptakan eco brick yang inovatif dan
-              berdaya guna! <br /> Eco brick dapat dimanfaatkan sebagai bahan
-              bangunan alternatif, furnitur kreatif, atau bahkan karya seni
-              ramah lingkungan.
+            <p className=" text-sm font-semibold max-w-2xl">
+              Sampah galon air mineral yang kita minum sehari-hari dapat kita
+              manfaatkan untuk pertanian mikro dengan memanfaatkannya menjadi
+              pot untuk tanaman
             </p>
           </div>
 
           <div>
             <h2>ALAT DAN BAHAN</h2>
-            <div className="grid grid-cols-4 w-full mt-5 justify-center gap-4 text-sm font-semibold">
+            <div className="grid grid-cols-3 w-full mt-5 justify-center gap-4 text-sm font-semibold px-20">
               <div className="flex flex-col items-center gap-2 ">
                 <img
-                  src={GG3}
+                  src={galon}
                   className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div>botol plastik</div>
+                <div>Galon</div>
               </div>
               <div className="flex flex-col items-center gap-2 ">
                 <img
-                  src={GG3}
+                  src={pisau}
                   className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div>sampah plastik </div>
+                <div>Pisau </div>
               </div>
               <div className="flex flex-col items-center gap-2 ">
                 <img
-                  src={GG3}
+                  src={solder}
                   className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div>gunting </div>
-              </div>
-              <div className="flex flex-col items-center gap-2 ">
-                <img
-                  src={GG3}
-                  className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
-                />
-                <div>tongkat</div>
+                <div>Solder</div>
               </div>
             </div>
           </div>
 
           <div>
             <h2>LANGKAH-LANGKAH</h2>
-            <div className="grid grid-cols-3 w-full mt-5 gap-y-4 gap-x-8 text-sm font-semibold">
+            <div className="grid grid-cols-3 w-full mt-5 gap-y-4 gap-x-8 text-sm font-semibold px-10">
               <div className="flex flex-col items-center gap-2">
                 <img
-                  src={GG3}
+                  src={tani1}
                   className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div>Kumpulkan sampah plastik dan botol mineral</div>
+                <div className="px-10">Lubangi bagian atas dengan solder</div>
               </div>
               <div className="flex flex-col items-center gap-2 ">
                 <img
-                  src={GG3}
+                  src={tani2}
                   className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div>Bersihkan dan jemur sampah plastik dan botol mineral </div>
+                <div className="px-10">
+                  Potong menjadi 2 bagian dengan pisau
+                </div>
               </div>
               <div className="flex flex-col items-center gap-2 ">
                 <img
-                  src={GG3}
+                  src={tani3}
                   className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div>Potong sampah plastik menjadi bagian-bagian kecil </div>
+                <div className="px-10">Hasil setelah dipotong</div>
               </div>
               <div className="flex flex-col items-center gap-2  ">
                 <img
-                  src={GG3}
+                  src={tani4}
                   className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div>
-                  Masukan potongan sampah ke dalam botol dengan bantuan tongkat
+                <div className="px-10">
+                  Susun kedua bagian sesuai gambar di atas
                 </div>
               </div>
               <div className="flex flex-col items-center gap-2  ">
                 <img
-                  src={GG3}
+                  src={tani4}
                   className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div>Pastikan isi dalam botol terkompresi</div>
+                <div className="px-10">
+                  Isi bagian bawah dengan air, dan bagian atas dengan media
+                  tanam
+                </div>
               </div>
               <div className="flex flex-col items-center gap-2  ">
                 <img
-                  src={GG3}
+                  src={tani4}
                   className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div>Hasil akhir eco brick</div>
+                <div className="px-10">Tanam tanaman yang anda inginkan</div>
               </div>
             </div>
           </div>
 
-          <div>
+          <div className="w-full flex flex-col items-center">
             <h2>THANK YOU!</h2>
-            <div className="text-sm font-bold">
-              Mari manfaatkan sampah tersebut untuk memberikan kontribusi
-              positif <br />
-              bagi lingkungan dan menciptakan perubahan yang berkelanjutan.
+            <div className="text-sm font-semibold max-w-2xl ">
+              Jangan biarkan sampah terbuang sia-sia, ayo bergandengan tangan
+              dalam memanfaatkannya untuk menciptakan dunia yang lebih hijau dan
+              berkelanjutan!
             </div>
           </div>
         </div>
