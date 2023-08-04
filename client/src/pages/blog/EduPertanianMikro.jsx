20b555927c62f9594fa0a024e2511a9503d7eab0
usernamemu <mhmdarif.developer@gmail.com>
2023-08-04 15:20:36 +0700

    responsive edupertanian

--- client/src/pages/blog/EduPertanianMikro.jsx
+++ client/src/pages/blog/EduPertanianMikro.jsx
@@ -19,13 +19,13 @@ const EduPertanianMikro = () => {
       <Navbar />
       <div
         id="Body"
-        className=" w-full flex flex-col container mx-auto font-poppins items-center lg:text-center lg:mt-32 font-bold max-w-5xl"
+        className=" w-full flex flex-col container mx-auto font-poppins items-center lg:text-center lg:mt-32 mt-24  font-bold max-w-5xl"
       >
-        <h1 className="text-4xl mb-10">PERTANIAN MIKRO</h1>
-        <div className="max-w-4xl flex flex-col gap-14">
+        <h1 className="lg:text-4xl text-3xl mb-10">PERTANIAN MIKRO</h1>
+        <div className="max-w-4xl flex flex-col gap-14 mx-5">
           <div className="flex flex-col gap-2 items-center">
             <h2>TAUKAH KAMU?</h2>
-            <p className=" text-sm font-semibold max-w-2xl">
+            <p className=" text-sm text-center lg:font-semibold font-medium max-w-2xl">
               Sampah galon air mineral yang kita minum sehari-hari dapat kita
               manfaatkan untuk pertanian mikro dengan memanfaatkannya menjadi
               pot untuk tanaman
@@ -33,26 +33,26 @@ const EduPertanianMikro = () => {
           </div>
 
           <div>
-            <h2>ALAT DAN BAHAN</h2>
-            <div className="grid grid-cols-3 w-full mt-5 justify-center gap-4 text-sm font-semibold px-20">
+            <h2 className="text-center ">ALAT DAN BAHAN</h2>
+            <div className="grid lg:grid-cols-4 grid-cols-1 w-full mt-5 justify-center gap-4 text-sm font-semibold lg:px-20">
               <div className="flex flex-col items-center gap-2 ">
                 <img
                   src={galon}
-                  className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
+                  className="w-[200px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
                 <div>Galon</div>
               </div>
               <div className="flex flex-col items-center gap-2 ">
                 <img
                   src={pisau}
-                  className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
+                  className="w-[200px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
                 <div>Pisau </div>
               </div>
               <div className="flex flex-col items-center gap-2 ">
                 <img
                   src={solder}
-                  className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
+                  className="w-[200px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
                 <div>Solder</div>
               </div>
@@ -60,46 +60,48 @@ const EduPertanianMikro = () => {
           </div>
 
           <div>
-            <h2>LANGKAH-LANGKAH</h2>
-            <div className="grid grid-cols-3 w-full mt-5 gap-y-4 gap-x-8 text-sm font-semibold px-10">
+            <h2 className="text-center ">LANGKAH-LANGKAH</h2>
+            <div className="grid lg:grid-cols-3 grid-cols-1 w-full mt-5 gap-y-4 gap-x-8 text-sm font-semibold px-10">
               <div className="flex flex-col items-center gap-2">
                 <img
                   src={tani1}
-                  className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
+                  className="w-[200px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div className="px-10">Lubangi bagian atas dengan solder</div>
+                <div className="px-10 text-center">
+                  Lubangi bagian atas dengan solder
+                </div>
               </div>
               <div className="flex flex-col items-center gap-2 ">
                 <img
                   src={tani2}
-                  className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
+                  className="w-[200px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div className="px-10">
+                <div className="px-10 text-center">
                   Potong menjadi 2 bagian dengan pisau
                 </div>
               </div>
               <div className="flex flex-col items-center gap-2 ">
                 <img
                   src={tani3}
-                  className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
+                  className="w-[200px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div className="px-10">Hasil setelah dipotong</div>
+                <div className="px-10 text-center">Hasil setelah dipotong</div>
               </div>
               <div className="flex flex-col items-center gap-2  ">
                 <img
                   src={tani4}
-                  className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
+                  className="w-[200px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div className="px-10">
+                <div className="px-10 text-center">
                   Susun kedua bagian sesuai gambar di atas
                 </div>
               </div>
               <div className="flex flex-col items-center gap-2  ">
                 <img
                   src={tani4}
-                  className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
+                  className="w-[200px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div className="px-10">
+                <div className="px-10 text-center">
                   Isi bagian bawah dengan air, dan bagian atas dengan media
                   tanam
                 </div>
@@ -107,16 +109,18 @@ const EduPertanianMikro = () => {
               <div className="flex flex-col items-center gap-2  ">
                 <img
                   src={tani4}
-                  className="w-[150px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
+                  className="w-[200px] object-cover rounded-lg transform ease duration-300 hover:scale-[1.02] hover:shadow-md"
                 />
-                <div className="px-10">Tanam tanaman yang anda inginkan</div>
+                <div className="px-10 text-center">
+                  Tanam tanaman yang anda inginkan
+                </div>
               </div>
             </div>
           </div>
 
           <div className="w-full flex flex-col items-center">
             <h2>THANK YOU!</h2>
-            <div className="text-sm font-semibold max-w-2xl ">
+            <div className="text-sm lg:font-semibold font-medium max-w-2xl text-center ">
               Jangan biarkan sampah terbuang sia-sia, ayo bergandengan tangan
               dalam memanfaatkannya untuk menciptakan dunia yang lebih hijau dan
               berkelanjutan!
